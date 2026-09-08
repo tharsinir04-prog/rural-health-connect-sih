4d696e6cbebb7aeb600135b2771c557b05a59151
tharsinir04-prog <tharsinir09@gmail.com>
2026-09-08 19:33:26 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request) {
+    return new Response("Rural Health Connect SIH is starting 🚀", {
+      headers: {
+        "content-type": "text/plain"
+      }
+    });
+  }
+};
